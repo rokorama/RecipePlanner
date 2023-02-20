46b60b39d2b9c60ae1bd37df92b609bead446e94
Rokas <rokamanauskas@gmail.com>
2023-02-20 23:00:37 +0200

    Change data type for Images

---
+++ Server/Migrations/20230220205810_Change-Image-data-type.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipePlanner.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeImagedatatype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Recipes_RecipeId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_RecipeId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "Images");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "Recipes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("2a3d6c16-98f9-47bf-ad3a-5ed26ec20651"),
+                columns: new[] { "DateCreated", "ImageId" },
+                values: new object[] { new DateTime(2023, 2, 20, 0, 0, 0, 0, DateTimeKind.Local), null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_ImageId",
+                table: "Recipes",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Images_ImageId",
+                table: "Recipes",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Images_ImageId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_ImageId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Recipes");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Data",
+                table: "Images",
+                type: "varbinary(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RecipeId",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("2a3d6c16-98f9-47bf-ad3a-5ed26ec20651"),
+                column: "DateCreated",
+                value: new DateTime(2023, 2, 19, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_RecipeId",
+                table: "Images",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Recipes_RecipeId",
+                table: "Images",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Shared/Image.cs
+++ Shared/Image.cs
@@ -5,9 +5,5 @@ namespace RecipePlanner.Shared;
 public class Image
 {
     public Guid Id { get; set; }
-    [JsonIgnore]
-    public Recipe Recipe { get; set;} = new();
-    public Guid RecipeId { get; set; }
-    public byte[] Data { get; set; } = new byte[]{};
-
+    public string Data { get; set; } = string.Empty;
 }
