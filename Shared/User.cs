9559976f4da34727f7949ddf67e346e2d00c2db0
Rokas <rokamanauskas@gmail.com>
2023-03-30 00:13:43 +0300

    Add propertys to User & DB migration

--- Shared/User.cs
+++ Shared/User.cs
@@ -2,5 +2,12 @@ namespace RecipePlanner.Shared;
 
 public class User
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public List<Recipe> SavedRecipes { get; set; } = new();
+    public List<Recipe> UploadedRecipes { get; set; } = new();
+    public DateTime DateCreated { get; set; } = DateTime.Today;
+    public string Role { get; set; } = "user";
 }
