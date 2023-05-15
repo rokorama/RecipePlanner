353cc2eb317edb49985442ac431417a5074b4e11
Rokas <rokamanauskas@gmail.com>
2023-05-15 22:20:20 +0300

    Implement saving Recipes for Users, associated migrations and bug fixes

---
+++ Server/Migrations/20230513080536_Bug-fixes.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipePlanner.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Bugfixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Users_UploadedById",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_UploadedById",
+                table: "Recipes");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Recipes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("2a3d6c16-98f9-47bf-ad3a-5ed26ec20651"),
+                columns: new[] { "DateCreated", "UserId" },
+                values: new object[] { new DateTime(2023, 5, 13, 0, 0, 0, 0, DateTimeKind.Local), null });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("9841dbcf-02a4-4be6-9545-35aff7db9c7b"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 13, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Users_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Users_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Recipes");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("2a3d6c16-98f9-47bf-ad3a-5ed26ec20651"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 4, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("9841dbcf-02a4-4be6-9545-35aff7db9c7b"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 4, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UploadedById",
+                table: "Recipes",
+                column: "UploadedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Users_UploadedById",
+                table: "Recipes",
+                column: "UploadedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Server/Migrations/20230515184347_Adjust-UserRecipe-configuration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipePlanner.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustUserRecipeconfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRecipes_Users_UserId",
+                table: "UserRecipes");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("2a3d6c16-98f9-47bf-ad3a-5ed26ec20651"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 15, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("9841dbcf-02a4-4be6-9545-35aff7db9c7b"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 15, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRecipes_Users_UserId",
+                table: "UserRecipes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRecipes_Users_UserId",
+                table: "UserRecipes");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("2a3d6c16-98f9-47bf-ad3a-5ed26ec20651"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 13, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("9841dbcf-02a4-4be6-9545-35aff7db9c7b"),
+                column: "DateCreated",
+                value: new DateTime(2023, 5, 13, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRecipes_Users_UserId",
+                table: "UserRecipes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
